3d242ab293b43278a7a1922e68cb30b8704a4a6c
Anatol Kaczmarek <92335424+Tole-k@users.noreply.github.com>
2023-11-18 22:22:35 +0100

    almost

--- getResults.sh
+++ getResults.sh
@@ -13,13 +13,14 @@ alfa=(1 2 4 8)
 beta=(1 2 4)
 gamma=(1 2 4)
 p=(0.01 0.025 0.05)
+max=0
 
 for i in ${alfa[@]}; do
     for j in ${beta[@]}; do
         for k in ${gamma[@]}; do
             for l in ${p[@]}; do
                 res=0
-                for path in Round/*; do
+                for path in Round/f_*; do
                     filename=$(basename $path .txt)
                     output=$(./solutionTester $path output/$filename"-"$i"_"$j"_"$k"_"$l.out)
                     ((res=res+$(echo $output | awk '{print $NF}')))
@@ -27,7 +28,13 @@ for i in ${alfa[@]}; do
                 done
                 echo $i $j $k $l
                 echo $res
+                if [[ res -ge max ]]; then
+                    max=$res
+                    maxParams=$i"_"$j"_"$k"_"$l
+                fi
             done
         done
     done
 done
+
+echo "max: " $max $maxParams
